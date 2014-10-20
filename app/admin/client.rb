2368226962345dddb80386753c05901d29c395af
Tomás Mateus <tmsmateus@gmail.com>
2014-10-20 20:00:29 -0300

    user create

--- app/admin/client.rb
+++ app/admin/client.rb
@@ -31,27 +31,31 @@ ActiveAdmin.register Client do
       @user = User.new( params[:client][:user_attributes] )
       @user.user_role_id = 2
       if !@user.save
-        redirect_to new_admin_client_path , notice: 'El cliente no ha podido crearse.'
-      end
-
-      @client = Client.new
-      @client.first_name = params[:client][:first_name]
-      @client.last_name =  params[:client][:last_name]
-      @client.user_id = @user.id
-      @client.newsletter_frequency_id = 3
-      @client.save!
-      if !@client.save
         redirect_to new_admin_client_path, notice: 'El cliente no ha podido crearse.'
+      else
+        @client = Client.new
+        @client.first_name = params[:client][:first_name]
+        @client.last_name =  params[:client][:last_name]
+        @client.user_id = @user.id
+        @client.newsletter_frequency_id = 3
+        @client.save!
+        if !@client.save
+          @user.delete
+          redirect_to new_admin_client_path, notice: 'El cliente no ha podido crearse.'
+        else
+          address_params = params[:client][:address_attributes]
+          @address = Address.new( params[:client][:address_attributes] )
+          @address.client_id = @client.id
+          if address_params[:street] == "" || address_params[:number] == "" || !@address.save
+            @client.delete
+            @user.delete
+            redirect_to new_admin_client_path, notice: 'El cliente no ha podido crearse.'
+          else
+            NewUserMailer.new_client_email(@client.user).deliver
+            redirect_to admin_clients_path
+          end
+        end
       end
-      NewUserMailer.new_client_email(@client.user).deliver
-
-
-      @address = Address.new( params[:client][:address_attributes] )
-      @address.client_id = @client.id
-      @address.save
-
-      redirect_to admin_clients_path
-
     end
   end
 
