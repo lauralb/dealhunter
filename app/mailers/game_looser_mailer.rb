71aa6c9c4c90d5f855e06839154d1ff1f5d8d167
francotestori <franco.testori@hotmail.com>
2014-10-23 11:59:10 -0300

    DEAL-016 Agregamos el looser mailer

--- app/mailers/game_looser_mailer.rb
+++ app/mailers/game_looser_mailer.rb
@@ -1,7 +1,12 @@
 class GameLooserMailer < ActionMailer::Base
+  default :from => "noresponder@dealhunter.com"
 
-  def new_message
-
+  def game_looser_mailer(user,offer,position,other_offers)
+    @user = user
+    @offer = offer
+    @position = position
+    @offers = other_offers
+    mail(:to => 'nschejtman@hotmail.com', :subject => 'Dealhunter: Resultado de Oferta!')
   end
 
 
--- app/mailers/game_winner_mailer.rb
+++ app/mailers/game_winner_mailer.rb
@@ -1,9 +1,13 @@
 class GameWinnerMailer < ActionMailer::Base
+  default :from => "noresponder@dealhunter.com"
 
-  def new_message
-
-
+  def game_winner_email(user, offer, position, prize, other_offers)
+    @user = user
+    @offer = offer
+    @prize = prize
+    @position = position
+    @offers = other_offers
+    mail(:to => 'nschejtman@hotmail.com', :subject => 'Has ganado!')
   end
-
 
 end
