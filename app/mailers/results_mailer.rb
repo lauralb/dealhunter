8e3fc8b81335f0f51cf247578ed022f7793e8637
manuelcernello <manuelcernello@gmail.com>
2014-10-27 19:43:07 -0300

    DEAL-017 envia emails / correciones de bugs

--- app/mailers/results_mailer.rb
+++ app/mailers/results_mailer.rb
@@ -1,12 +1,17 @@
 class ResultsMailer < ActionMailer::Base
   default :from =>  "noresponder@dealhunter.com"
 
-  def results_mailer(company, offer)
+  def results_email(company, offer)
 
     @offer = offer
-    @prizes = Prize.all.delete_if{|x| x.offer != offer}
+    @prizes = Prize.all.delete_if {|x| x.offer_id != offer.id}
     @company = company
-    @clients = ClientsOffer.all.delete_if{|x| x.offer != offer && x.participated.nil?}.client
+    client_offers = ClientsOffer.all.delete_if{|x| x.offer_id != offer.id && x.participated.nil?}
+    @clients= []
+    client_offers.each do |co|
+      @clients << co.client
+    end
+
 
     mail(:to => company.email, :subject => "Resumen de oferta #{offer.name}")
   end
